7bd701fb69dd929b4b9d2e6e6100cfdef60acaf6
ChristophGreger <70233504+ChristophGreger@users.noreply.github.com>
2024-01-05 01:22:59 +0100

    Performance Update
    Big Performance Update by renaming all the unnecessary variables to _var for not streaming them to the frontend

--- AlgoPage/pathFindingState.py
+++ AlgoPage/pathFindingState.py
@@ -6,39 +6,39 @@ from .pathFindingGrids import getGridwithConfiguration
 
 
 class PathFindingState(rx.State):
-    start: Tuple[int, int] = (0, 0)
-    end: Tuple[int, int] = (19, 19)
+    _start: Tuple[int, int] = (0, 0)
+    _end: Tuple[int, int] = (19, 19)
 
-    currentlysetting: str = "start"  # "start" if setting start, "end" if setting end, "barrier" if setting barrier
-    distancematrix: List[List[int]] = [[1000 for i in range(20)] for j in range(20)]
-    distancematrix[start[0]][start[1]] = 0
-    finished: Set[Tuple[int, int]] = set()
-    currentlyopen: Set[Tuple[int, int]] = set()
+    _currentlysetting: str = "start"  # "start" if setting start, "end" if setting end, "barrier" if setting barrier
+    _distancematrix: List[List[int]] = [[1000 for i in range(20)] for j in range(20)]
+    _distancematrix[_start[0]][_start[1]] = 0
+    _finished: Set[Tuple[int, int]] = set()
+    _currentlyopen: Set[Tuple[int, int]] = set()
 
     fieldmatrix: List[List[str]] = [["grey" for i in range(20)] for j in range(
         20)]  # "grey", "red", "green", "blue", "yellow" für normalbutton, endingbutton, startingbutton, pathbutton, searchbutton
-    fieldmatrix[start[0]][start[1]] = "green"
-    fieldmatrix[end[0]][end[1]] = "red"
+    fieldmatrix[_start[0]][_start[1]] = "green"
+    fieldmatrix[_end[0]][_end[1]] = "red"
 
     def resetGrid(self):
-        self.start = (0, 0)
-        self.end = (19, 19)
+        self._start = (0, 0)
+        self._end = (19, 19)
         self.fieldmatrix: List[List[str]] = [["grey" for i in range(20)] for j in range(
             20)]  # "grey", "red", "green", "blue", "yellow" für normalbutton, endingbutton, startingbutton, pathbutton, searchbutton
-        self.fieldmatrix[self.start[0]][self.start[1]] = "green"
-        self.fieldmatrix[self.end[0]][self.end[1]] = "red"
-        self.currentlysetting = "start"
-        self.distancematrix: List[List[int]] = [[1000 for i in range(20)] for j in range(20)]
-        self.distancematrix[self.start[0]][self.start[1]] = 0
-        self.finished: Set[Tuple[int, int]] = set()
-        self.currentlyopen: Set[Tuple[int, int]] = set()
+        self.fieldmatrix[self._start[0]][self._start[1]] = "green"
+        self.fieldmatrix[self._end[0]][self._end[1]] = "red"
+        self._currentlysetting = "start"
+        self._distancematrix: List[List[int]] = [[1000 for i in range(20)] for j in range(20)]
+        self._distancematrix[self._start[0]][self._start[1]] = 0
+        self._finished: Set[Tuple[int, int]] = set()
+        self._currentlyopen: Set[Tuple[int, int]] = set()
         self.isnotsolvable = False
 
     def resetSolve(self):
-        self.distancematrix: List[List[int]] = [[1000 for i in range(20)] for j in range(20)]
-        self.distancematrix[self.start[0]][self.start[1]] = 0
-        self.finished: Set[Tuple[int, int]] = set()
-        self.currentlyopen: Set[Tuple[int, int]] = set()
+        self._distancematrix: List[List[int]] = [[1000 for i in range(20)] for j in range(20)]
+        self._distancematrix[self._start[0]][self._start[1]] = 0
+        self._finished: Set[Tuple[int, int]] = set()
+        self._currentlyopen: Set[Tuple[int, int]] = set()
         self.isnotsolvable = False
         for i in range(20):
             for j in range(20):
@@ -48,14 +48,14 @@ class PathFindingState(rx.State):
                     self.fieldmatrix[i][j] = "grey"
 
     def setcurrentlysetting(self, value: str) -> None:
-        self.currentlysetting = value
+        self._currentlysetting = value
 
     def setStartandEndandBarrier(self, row: int, col: int) -> None:
-        if self.currentlysetting == "start":
+        if self._currentlysetting == "start":
             self.setStarting(row, col)
-        elif self.currentlysetting == "end":
+        elif self._currentlysetting == "end":
             self.setEnding(row, col)
-        elif self.currentlysetting == "barrier":
+        elif self._currentlysetting == "barrier":
             self.setBarrier(row, col)
 
     def setBarrier(self, row: int, col: int) -> None:
@@ -65,19 +65,19 @@ class PathFindingState(rx.State):
             self.fieldmatrix[row][col] = "grey"
 
     def setGridwithConfiguration(self, index: int) -> None:
-        self.start, self.end, self.fieldmatrix = getGridwithConfiguration(index)
+        self._start, self._end, self.fieldmatrix = getGridwithConfiguration(index)
         self.resetSolve()
 
     def setStarting(self, row: int, col: int) -> None:
-        self.fieldmatrix[self.start[0]][self.start[1]] = "grey"
-        self.distancematrix[self.start[0]][self.start[1]] = 1000
-        self.start = (row, col)
+        self.fieldmatrix[self._start[0]][self._start[1]] = "grey"
+        self._distancematrix[self._start[0]][self._start[1]] = 1000
+        self._start = (row, col)
         self.fieldmatrix[row][col] = "green"
-        self.distancematrix[row][col] = 0
+        self._distancematrix[row][col] = 0
 
     def setEnding(self, row: int, col: int) -> None:
-        self.fieldmatrix[self.end[0]][self.end[1]] = "grey"
-        self.end = (row, col)
+        self.fieldmatrix[self._end[0]][self._end[1]] = "grey"
+        self._end = (row, col)
         self.fieldmatrix[row][col] = "red"
 
     def getneighbors(self, pos: Tuple[int, int]) -> Set[Tuple[int, int]]:
@@ -94,37 +94,37 @@ class PathFindingState(rx.State):
         if pos[1] < 19:
             if self.fieldmatrix[pos[0]][pos[1] + 1] != "black":
                 neighbors.add((pos[0], pos[1] + 1))
-        if self.start in neighbors:
-            neighbors.remove(self.start)
+        if self._start in neighbors:
+            neighbors.remove(self._start)
         return neighbors
 
     def getlowestdistanceopen(self) -> Tuple[int, int]:
         lowest: Tuple[int, int] = (0, 0)
         lowestdistance: int = 1000
-        for coordinate in self.currentlyopen:
-            distance = self.distancematrix[coordinate[0]][coordinate[1]]
+        for coordinate in self._currentlyopen:
+            distance = self._distancematrix[coordinate[0]][coordinate[1]]
             if distance < lowestdistance:
                 lowestdistance = distance
                 lowest = coordinate
         return lowest
 
     def drawpathmatrix(self) -> None:
-        for x in self.getneighbors(self.justatuple):
-            if self.distancematrix[x[0]][x[1]] < self.distancematrix[self.justatuple[0]][self.justatuple[1]]:
-                self.justatuple = x
+        for x in self.getneighbors(self._justatuple):
+            if self._distancematrix[x[0]][x[1]] < self._distancematrix[self._justatuple[0]][self._justatuple[1]]:
+                self._justatuple = x
                 self.fieldmatrix[x[0]][x[1]] = "blue"
                 yield
                 yield from self.drawpathmatrix()
                 return
 
-    current: Tuple[int, int] = start
-    justatuple: Tuple[int, int] = end
+    _current: Tuple[int, int] = _start
+    _justatuple: Tuple[int, int] = _end
 
     isnotsolvable: bool = False
 
     async def solve(self) -> None:
-        self.current = self.start
-        self.justatuple = self.end
+        self._current = self._start
+        self._justatuple = self._end
         for i in self.solvehelp():
             yield
             await asyncio.sleep(0.02)
@@ -138,24 +138,24 @@ class PathFindingState(rx.State):
 
     # TODO Wenn es keinen Weg gibt, dann soll das Programm das anzeigen.
     def solvehelp(self) -> None:
-        currentdistance = self.distancematrix[self.current[0]][self.current[1]]
-        for coordinate in self.getneighbors(self.current):
-            coordinatedistance = self.distancematrix[coordinate[0]][coordinate[1]]
+        currentdistance = self._distancematrix[self._current[0]][self._current[1]]
+        for coordinate in self.getneighbors(self._current):
+            coordinatedistance = self._distancematrix[coordinate[0]][coordinate[1]]
             if coordinatedistance > currentdistance + 1:  # Hier stand mal ein plus 1
-                self.distancematrix[coordinate[0]][coordinate[1]] = currentdistance + 1
-                self.currentlyopen.add(coordinate)
+                self._distancematrix[coordinate[0]][coordinate[1]] = currentdistance + 1
+                self._currentlyopen.add(coordinate)
                 yield
                 if not self.fieldmatrix[coordinate[0]][coordinate[1]] == "red":
                     self.fieldmatrix[coordinate[0]][coordinate[1]] = "yellow"
-                if coordinate == self.end:
+                if coordinate == self._end:
                     return
-        self.finished.add(self.current)
-        if self.current in self.currentlyopen:
-            self.currentlyopen.remove(self.current)
-        if self.currentlyopen == set():
+        self._finished.add(self._current)
+        if self._current in self._currentlyopen:
+            self._currentlyopen.remove(self._current)
+        if self._currentlyopen == set():
             self.isnotsolvable = True
             return
-        self.current = self.getlowestdistanceopen()
+        self._current = self.getlowestdistanceopen()
         yield from self.solvehelp()
 
     def printGrid(self) -> None:
