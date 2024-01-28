a9f904bc704ff22f84b2c850ac1de3c68662cac7
Christoph <cgreger1@t-online.de>
2024-01-28 21:22:54 +0100

    maybe it works now ?

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,8 @@
 source /home/AlgoPage/.venv/bin/activate
 cd /home/AlgoPage
 echo "Starting ReflexPage"
-# screen -AmdS reflexpage reflex run
+screen -AmdS reflexpage reflex run
 echo "ReflexPage started!"
+while true; do
+    sleep 1
+done
