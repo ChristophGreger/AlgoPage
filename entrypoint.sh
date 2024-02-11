83949e790a630659c436137fe2a9fd311f6d3550
ChristophGreger <70233504+ChristophGreger@users.noreply.github.com>
2024-02-11 14:02:24 +0100

    Revert "Trying to improve performance"
    This reverts commit d56ae27ffb28f2ca40f1a396a5949b7cc4983dd7.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 source /home/AlgoPage/.venv/bin/activate
 cd /home/AlgoPage
 echo "Starting ReflexPage"
-screen -AmdS reflexpage reflex run --env prod
+screen -AmdS reflexpage reflex run
 echo "ReflexPage started!"
 while true; do
     sleep 1
